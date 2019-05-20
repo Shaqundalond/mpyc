73e6814f2be2d9aa8f711f4acaaa9bdf6bb598ae
Fabian Pfaff <pfafff@dhbw-loerrach.de>
2019-05-20 15:22:43 +0200

    made first interface design that is resizeable

---
+++ mpd_test.py
@@ -0,0 +1,19 @@
+#!/bin/python
+import mpd
+
+# use_unicode will enable the utf-8 mode for python2
+# see https://python-mpd2.readthedocs.io/en/latest/topics/advanced.html#unicode-handling
+client = mpd.MPDClient(use_unicode=True)
+client.connect("localhost", 6600)
+
+#for entry in client.lsinfo("/"):
+#    print("%s" % entry)
+#for key, value in client.status().items():
+#    print("%s: %s" % (key, value))
+
+for lel in client.playlist():
+    print(lel)
+
+client.previous()
+
+client.disconnect()
